d3965c943f96be68dd0affda42a719b800f1ae9b
lucasbarbaro <sbarbaroluca99@gmail.com>
2021-06-21 15:01:01 +0200

    scheletro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+        Header,
+        Main,
+        Footer
   }
 }
 </script>
